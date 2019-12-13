ef650dfbaeab056b8382ddaa8cc77050185bd3ad
SilviaTodorova <silviatdivanova@abv.bg>
2019-12-13 06:43:27 +0200

    connect to api - get results from all tests by specific user

--- src/app/component/results/results.component.ts
+++ src/app/component/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExamResults } from 'src/app/models/results/exam-results';
+import { TeachersService } from 'src/app/services/teachers.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ngbd-rating',
@@ -10,23 +12,26 @@ import { ExamResults } from 'src/app/models/results/exam-results';
 export class ResultsComponent implements OnInit{
   exams: ExamResults[] = [];
 
-  ngOnInit(){
-    let e1: ExamResults = new ExamResults();
-    e1.countQuestions = 10;
-    e1.average = 5.5;
-    e1.title = "XML технологии за семантичен Уеб";
-    let e2: ExamResults = new ExamResults();
-    e2.countQuestions = 50;
-    e2.average = 5.5;
-    e2.title = "Анализ на софтуерните изисквания";
-    let e3: ExamResults = new ExamResults();
-    e3.countQuestions = 30;
-    e3.average = 4.5;
-    e3.title = "Увод в софтуерното инженерство";
-    let e4: ExamResults = new ExamResults();
-    e4.countQuestions = 15;
-    e4.average = 2.5;
-    e4.title = "Софтуерни архитектури и разработка на софтуер";
-    this.exams = [e1,e2,e3,e4];
+  constructor(private router: Router,
+              private teachersService: TeachersService) { }
+
+  ngOnInit() {
+    this.loadExams();
+  }
+
+  //Serivces
+  loadExams(){
+    this.teachersService.getAllTestByOwner().subscribe(data => {
+      this.exams = data;
+
+      this.exams.map(x=>x.average = 0);
+      console.log(this.exams);
+    }, error => {
+      if(error.error.message){
+        alert(error.error.message);
+      }
+      this.router.navigate(['/dashboard']);
+    });
+
   }
 }
